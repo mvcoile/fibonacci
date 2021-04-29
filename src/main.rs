1127f1e45a111aec9e93774172f1a34b04765752
Maarten Van Coile <maarten.vancoile@me.com>
2021-04-29 20:55:32 +0200

    use BigUint instead of u128

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,33 @@ https://www.nayuki.io/page/fast-fibonacci-algorithms
 
 Fast doubling:
 */
+use num_bigint::{BigUint, ToBigUint};
+use num_traits::{Zero, One};
+
 fn main() {
-    let f: u128 = 128;
+    let f: usize = 4096;
 
     let now = std::time::Instant::now();
-    println!("fib {}: {} ({} ns)", f, fibonacci(f), now.elapsed().as_nanos());
+    println!("fib {}: {} ({} microseconds)", f, fibonacci(f), now.elapsed().as_micros());
 
 }
 
-fn fibonacci(n: u128) -> u128 {
-    _fib(n).0
+fn fibonacci(n: usize) -> BigUint {
+    _fib(n.to_biguint().unwrap()).0
 }
 
-fn _fib(n: u128) -> (u128, u128) {
-    if n == 0 {
-        (0,1)
+fn _fib(n: BigUint) -> (BigUint, BigUint) {
+    if n.is_zero() {
+        return (Zero::zero(), One::one())
     } else {
-        let (a, b) = _fib(n / 2); // ignoring remainder. eg.17/7 = 2
-        let c: u128 = a * (b * 2 - a);
-        let d: u128 = a * a + b * b;
-        if n % 2 == 0 {
-            (c, d)
+        let (a, b) = _fib(&n / 2.to_biguint().unwrap()); // ignoring remainder. eg.17/7 = 2
+        let c: BigUint = &a * (&b * 2.to_biguint().unwrap() - &a);
+        let d: BigUint = a.pow(2) + b.pow(2);
+
+        if n % 2.to_biguint().unwrap() == Zero::zero() {
+            return (c, d)
         } else {
-            (d, c+d)
+            return (d.clone() , c+d)
         }
     }
 }
