d208772b5c7138c630cc1ce90263a3b00571f709
Maarten Van Coile <maarten.vancoile@me.com>
2021-05-17 18:50:37 +0000

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,13 @@ https://www.nayuki.io/page/fast-fibonacci-algorithms
 Fast doubling:
 */
 use num_bigint::{BigUint, ToBigUint};
-use num_traits::{Zero, One};
+use num_traits::{One, Zero};
 
 fn main() {
     let f: usize = 4096;
 
     let now = std::time::Instant::now();
     println!("fib {}: {} ({} microseconds)", f, fibonacci(f), now.elapsed().as_micros());
-
 }
 
 fn fibonacci(n: usize) -> BigUint {
@@ -24,16 +23,16 @@ fn fibonacci(n: usize) -> BigUint {
 
 fn _fib(n: BigUint) -> (BigUint, BigUint) {
     if n.is_zero() {
-        return (Zero::zero(), One::one())
+        return (Zero::zero(), One::one());
     } else {
-        let (a, b) = _fib(&n / 2.to_biguint().unwrap()); // ignoring remainder. eg.17/7 = 2
+        let (a, b) = _fib(&n / 2.to_biguint().unwrap()); // ignoring remainder. eg. 17/7 = 2
         let c: BigUint = &a * (&b * 2.to_biguint().unwrap() - &a);
         let d: BigUint = a.pow(2) + b.pow(2);
 
         if n % 2.to_biguint().unwrap() == Zero::zero() {
-            return (c, d)
+            return (c, d);
         } else {
-            return (d.clone() , c+d)
+            return (d.clone(), c + d);
         }
     }
 }
